8cfc7eeaf3e10e049b5cd6748314618d1e9bd6c4
Bipasssa <bipassa11@gmail.com>
2022-06-01 00:52:15 +0530

    new merge

---
+++ src/com/sellabs/querty/Sample.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.sellabs.querty;
+
+/**
+ * @author bipas
+ *
+ */
+public class Sample {
+	
+	
+		public static void main(String[] args) {
+			Sample s = new Sample();
+			s.calculate();
+		}
+	
+	
+	public void calculate(){
+		
+		String s1 = "Downloads | Selenium";
+		String s2 = s1.replace("| ","");
+		String s3=s2.toLowerCase();
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		
+	
+	
+	
+	}
+	
+
+}
